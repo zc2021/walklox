7937634fb918e1b80f0d00a25ffc37e786b77e37
Zoey Carter <carter.zo3y@gmail.com>
2022-09-30 17:20:29 -0700

    skeleton for exprs generation

---
+++ internal/tools/gen_expr.go
@@ -0,0 +1,10 @@
+// The following build directive is necessary to make the package coherent:
+
+//go:build ignore
+
+// This program generates Expr structs. It can be invoked using go generate.
+package main
+
+func main() {
+
+}
